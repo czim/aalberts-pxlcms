99c50348623d6d2299ed6595117b20e865d45d3b
Coen Zimmerman <coen@pixelindustries.com>
2016-06-11 12:52:36 +0200

    added cachetags enum

---
+++ src/Enums/CacheTags.php
@@ -0,0 +1,19 @@
+<?php
+namespace Aalberts\Enums;
+
+use MyCLabs\Enum\Enum;
+
+class CacheTags extends Enum
+{
+    
+    const TRANSLATION = 'translation';
+
+    const PRODUCT     = 'product';
+    const TOP_PRODUCT = 'top-product';
+
+    const CONTENT  = 'content';
+    const NEWS     = 'news';
+    const PROJECT  = 'project';
+    const DOWNLOAD = 'download';
+
+}
