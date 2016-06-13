3139947e979e0bf7705efb7ba214d1371ce48c5c
Coen Zimmerman <coen@pixelindustries.com>
2016-06-13 18:44:32 +0200

    presenter: added normalized methods for abstract

--- src/Models/Presenters/AbstractPresenter.php
+++ src/Models/Presenters/AbstractPresenter.php
@@ -1,9 +1,44 @@
 <?php
 namespace Aalberts\Models\Presenters;
 
+use Carbon\Carbon;
 use Laracasts\Presenter\Presenter;
 
 abstract class AbstractPresenter extends Presenter
 {
 
+    /**
+     * @param null|Carbon $date
+     * @return null|string
+     */
+    protected function normalizeDate($date)
+    {
+        if ( ! $date) return null;
+
+        return $date->format('Y-m-d');
+    }
+    
+    /**
+     * @param null|Carbon $date
+     * @return null|string
+     */
+    protected function normalizeDateTime($date)
+    {
+        if ( ! $date) return null;
+
+        return $date->format('Y-m-d H:i');
+    }
+
+    /**
+     * @param string $url
+     * @return string
+     */
+    protected function normalizeLink($url)
+    {
+        if ( ! $url) return '';
+
+        return (( ! preg_match('#^(https?:)?//#i', $url)) ? 'http://' : '')
+        . $url;
+    }
+    
 }
