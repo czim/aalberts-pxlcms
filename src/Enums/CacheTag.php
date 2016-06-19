2d5d24d01b95e9f6dc79466a2c4b2a1e89421036
Coen Zimmerman <coen@pixelindustries.com>
2016-06-19 17:41:37 +0200

    enum: cachetag expanded/cleaned up

--- src/Enums/CacheTag.php
+++ src/Enums/CacheTag.php
@@ -5,15 +5,23 @@ use MyCLabs\Enum\Enum;
 
 class CacheTag extends Enum
 {
-    
-    const TRANSLATION = 'translation';
-    const COUNTRY     = 'country';
+
+    // ------------------------------------------------------------------------------
+    //      Compano
+    // ------------------------------------------------------------------------------
 
     const CMP_PRODUCT     = 'cmp-product';
     const CMP_SUPPLIER    = 'cmp-supplier';
     const CMP_MISC        = 'cmp-misc';     // pumpbrand, measurements, random stuff (catch-all)
 
-    const TOP_PRODUCT = 'top-product';
+
+    // ------------------------------------------------------------------------------
+    //      CMS
+    // ------------------------------------------------------------------------------
+
+    const TRANSLATION = 'translation';
+    const COUNTRY     = 'country';
+    const LANGUAGE    = 'language';
 
     const CONTENT      = 'content';
     const NEWS         = 'news';
@@ -27,5 +35,18 @@ class CacheTag extends Enum
     // combined all the press dimensions, tools, remarks, etc
     const PRESS = 'press';
 
+    const PRODUCTGROUP = 'productgroup';
+    const FILTERGROUP  = 'filtergroup';
+
+    // combined popular & highlighted product
+    const TOP_PRODUCT = 'top-product';
+
+    const APPROVAL    = 'approval';
+    const SOLUTION    = 'solution';
+    const APPLICATION = 'application';
+
+    const STORE = 'store';
+
+    const EXTERNALPROJECT = 'external-project';
 
 }
